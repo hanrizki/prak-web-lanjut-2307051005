802153590aa882fe5b23629b197220e7ec8c6adf
hanrizki <mvrvarhanrizky@gmail.com>
2025-03-10 01:33:55 +0700

    menambah form

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 
@@ -17,5 +18,10 @@ use App\Http\Controllers\ProfileController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/profile', [ProfileController::class, 'profile']);
 
 Route::get('/profile/{nama}/{kelas}/{npm}', [ProfileController::class, 'profile']);
+
+Route::get('/user/create', [UserController::class, 'create'])->name('user_create');
+
+Route::post('user/store', [UserController::class, 'store'])->name('user.store');
